e4f4b356b3ec61038d0948dc7fb346ff9a8bd7ae
developer2 <dev2@email.com>
2026-04-02 17:23:41 +0900

    Modify btn1, le1 and method name to setDisplay in ui.py

--- ui.py
+++ ui.py
@@ -16,6 +16,8 @@ class View(QWidget):
 
         self.le1 = QLineEdit('0', self) # 라인 에디트 위젯 생성
         self.le1.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트 위젯의 텍스트를 오른쪽 정렬
+        self.le1.setFocus() # 라인 에디트 위젯에 포커스 설정
+        self.le1.selectAll() # 라인 에디트 위젯의 텍스트 전체 선택
         
         self.le2 = QLineEdit('0', self) # 라인 에디트 위젯 생성
         self.le2.setAlignment(QtCore.Qt.AlignRight) # 라인 에디트 위
@@ -31,7 +33,7 @@ class View(QWidget):
         self.te1 = QPlainTextEdit() # 텍스트 에디트 위젯 생성
         self.te1.setReadOnly(True) # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
 
-        self.btn1=QPushButton('Message',self) # 버튼 추가
+        self.btn1=QPushButton('Calc',self) # 버튼 추가
         self.btn2=QPushButton('Clear',self) # 버튼 2 추가
 
         hbox = QHBoxLayout() # 수직 레이아웃 위젯 생성
@@ -52,8 +54,8 @@ class View(QWidget):
         self.setWindowIcon(QIcon('icon.png'))
         self.resize(256,256)
         self.show()
-
-    def activateMessage(self): # 버튼을 클릭할 때 동작하는 함수 : 메시지 박스 출력
+    
+    def setDisplay(self): # 버튼을 클릭할 때 동작하는 함수 : 메시지 박스 출력
         # QMessageBox.information(self, "information", "Button Clicked!")
         self.te1.appendPlainText("Button clicked!")
     
