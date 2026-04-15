8da48f96ee4b31ee7080602dcfacbe7cc087bc4d
redppgppc <44011318+redppgppc@users.noreply.github.com>
2026-04-15 18:37:15 +0900

    Add mod function and modify calculate function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -18,6 +18,8 @@ class Control:
             return f'{num1} / {num2} = {self.div(num1, num2)}'
         elif operator == '^':
             return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
+        elif operator == '%':
+            return f'{num1} % {num2} = {self.mod(num1, num2)}'
         else:
             return "Calculation Error"
         
@@ -53,3 +55,12 @@ class Control:
             return e
       
         return pow(a, b)
+    
+    def mod(self, a, b):
+        try:
+            if b == 0:
+                raise Exception("Modulo by zero is not allowed.")
+        except Exception as e:
+            return e
+        
+        return a % b
